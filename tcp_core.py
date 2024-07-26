bc817fd91a4590f8892f383392e2f6114dddbd4b
doudou <jeremychiambx@gmail.com>
2024-07-27 06:57:04 +0800

    added temp 0 model to decide if RAG is needed or not

--- tcp_core.py
+++ tcp_core.py
@@ -4,6 +4,8 @@ import socket
 import sys
 import re
 import os
+# need for first runn
+# import nltk 
 from ollama import AsyncClient
 from sentence_transformers import SentenceTransformer
 from custom_embedder import GenerateAllEmbeddings, KnnSearch
@@ -36,35 +38,41 @@ class TcpServer:
         # Interpret the User's Prompt and remove the unique terminator
         msg = buffer.decode()
         msg = msg[:-len(substring)]
-        msg = msg[1:]
         # need to find a way to extract the latest question
         # find the last occurence of the "content" which is the latest user prompt
+        host_string = "localhost:" + self.ollama_port
+        async_client = AsyncClient(host=host_string)
         prompt_index = msg.rfind('"content":')
         # shift past content:
         prompt_index += 10
         # remove the special characters from behind
-        question_embedding = model.encode([msg[prompt_index:len(msg)-2]])   
-        best_matches = KnnSearch(question_embedding, all_embeddings, k=5) # Play with this value, higher seems to result in inaccurate results
+        response = await async_client.generate(model="llama3-temp0-Q6", prompt=msg[prompt_index:len(msg)-2], system="Provide ONLY a single word response of Yes or No, Yes if you are able to provide an accurate and full answer or No if you are unable to provide an answer", stream=False)
 
-        #construct the embedding text
-        sourcetext = ""
-        for i, (index, source_text) in enumerate(best_matches, start=1):
-            sourcetext += f"{i}. Index: {index}, Source Text: {source_text}"
-        
-        # add on the system prompt to the front of the msg
-        sys_prompt_str = '[{"role": "system", "content": "Use this information if you are unable to provide an answer:' + sourcetext + '"},'
-        msg = sys_prompt_str + msg
+        if response['response'] != "Yes":
+            msg = msg[1:]
+            question_embedding = model.encode([msg[prompt_index:len(msg)-2]])
+            best_matches = KnnSearch(question_embedding, all_embeddings, k=5) # Play with this value, higher seems to result in inaccurate results
+
+            #construct the embedding text
+            sourcetext = ""
+            for i, (index, source_text) in enumerate(best_matches, start=1):
+                sourcetext += f"{i}. Index: {index}, Source Text: {source_text}"
+            
+            # add on the system prompt to the front of the msg
+            sys_prompt_str = '[{"role": "system", "content": "Try your best to answer the questions with your own knowledge and the context of the conversation, only if that is not possible then use the following information:' + sourcetext + '"},'
+            msg = sys_prompt_str + msg
+
         data = json.loads(msg)
 
         # Disabling Nagle's algorithm for the socket
         writer.get_extra_info('socket').setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        host_string = "localhost:" + self.ollama_port
-        async_client = AsyncClient(host=host_string)
 
         # tinyllama:1.1b-chat-v0.6-q6_K
         # llama3:8b-instruct-q6_K
-        async for part in await async_client.chat(model='llama3:8b-instruct-q6_K', messages=data, stream=True):
-            writer.write(part['message']['content'].encode('utf-8'))
+        async for part in await async_client.chat(model='llama3.1:8b-instruct-q8_0', messages=data, stream=True, keep_alive="15m"): # llama3:8b-instruct-q6_K
+            temp = part['message']['content']
+            writer.write(temp.encode('utf-8'))
+            print(temp)
             await writer.drain()
         
         token_count = part['eval_count']
@@ -84,6 +92,8 @@ class TcpServer:
 
 if __name__== "__main__":
     model = SentenceTransformer('all-MiniLM-L6-v2')
+    # needed for first run
+    #nltk.download('punkt')
     def validate_ip(ip):
         ip_pattern = r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
         if re.match(ip_pattern, ip):
