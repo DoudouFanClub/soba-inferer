5432f948f54807444afeef893c92c665b6e8adc4
l-junpu <leejunpu97@gmail.com>
2024-07-31 14:58:18 +0800

    Modified the way RAG performs its queries on the embeddings by adding a compressed prompt sequence prior to querying RAG, and updated readme

--- readme.md
+++ readme.md
@@ -1,12 +1,76 @@
-# LLM Inferer
+# Soba Inferer
+
+This project consists of 2 segments:
+- Soba Compressor / Embedder
+- Soba Inferer
+
+## Soba Compressor / Embedder
+
+`Soba Compressor` supports the reading and sentence splitting of `*.pdf` and `*.txt` files provided within a directory. It performs chunking of the retrieved data based on `sentence + word count` and generates a `compressed_*.txt` file in the `soba-inferer/compressed/*` directory.
+
+The compressed files are then subsequently passed to the `Soba Embedder` which performs embedding to convert it to a vector database for subsequent reference in a KNN search.
+
+Refer to the concept: [Hugging Face - Advanced Rag](https://huggingface.co/learn/cookbook/advanced_rag)
+
+## Soba Inferer
+
 This will serve as the LLM Inferer that will serve as the endpoint for the LLM Server.
 
-## Dependencies
-Currently the inferer requires `ollama` in order to function, this should be replaced in the future either using `llama_cpp`, `pytorch` or `transformers` by Hugging Face.
+## Installations
 
-## How to use
-Currently just using the following commands should allow for testing of the LLM Server
 ```python
-pip install ollama
-python tcp_core.py
+# You may be required to perform an installation within the code using the "nltk" library
+# before being able to run the tcp_core.py file (inferer)
+import nltk
+
+if __name__== "__main__":
+    nltk.download('punkt')
+```
+
+## How To Use
+
+### Dependency Installation
+
+```cmd
+pip install -r requirements.txt
+```
+
+### Standalone Data Compression
+
+```python
+from custom_compressor import GenerateCompressedFiles
+
+"""
+Reads all *.pdf and *.txt files from "your_file_directory" and
+writes them to "soba-inferer/compressed/*" where * represents
+the read file's prefix directory
+"""
+if __name__== "__main__":
+    GenerateCompressedFiles("your_file_directory")
+```
+
+### Standalone Data Embedding
+
+```python
+from custom_embedder import GenerateAllEmbeddings
+
+"""
+Creates a single embedding vector database using the 'all-MiniLM-L6-v2'
+model from SentenceTransformer after reading each "compressed_file" from
+"your_compressed_file_directory"
+"""
+if __name__== "__main__":
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    embeddings = GenerateAllEmbeddings("your_compressed_file_directory", model)
+```
+
+### Run Inferer
+
+```bash
+# Run the following command in a terminal
+# 192.168.0.1 - Refers to the IPV4 address that the inferer listens to
+# 7060        - Refers to the Port that the inferer listens to
+# 11434       - Refers to the Port that the ollama client listens to
+#               - Could be in Docker Container / Localhost
+python tcp_core.py 192.168.0.1 7060 11434
 ```
--- tcp_core.py
+++ tcp_core.py
@@ -15,72 +15,115 @@ class TcpServer:
     def __init__(self, host, port, ollama_port):
         self.host = host
         self.port = port
-        self.ollama_port = ollama_port
         self.server = None
         self.model = None
         self.embeddings = None
+        self.substring = "<doudousmacksoba>"
+        self.async_client = AsyncClient("localhost:" + ollama_port)
+
         print("Host: ", host, "  |  Port: ", port)
 
-    async def handle_client(self, reader, writer):
-        print("msg received")
-
+    async def await_user_prompt(self, reader):
         buffer = bytearray()
-        substring = "<doudousmacksoba>"
-
-        # Get the complete message first to pass to llm
         while True:
             data = await reader.read(1024)
             string_value = data.decode('utf-8')
             buffer.extend(data)
-            if substring in string_value:
-                break
-        
-        # Interpret the User's Prompt and remove the unique terminator
-        msg = buffer.decode()
-        msg = msg[:-len(substring)]
-        # need to find a way to extract the latest question
-        # find the last occurence of the "content" which is the latest user prompt
-        host_string = "localhost:" + self.ollama_port
-        async_client = AsyncClient(host=host_string)
+            if self.substring in string_value:
+                # Interpret the User's Prompt and remove the unique terminator
+                msg = buffer.decode()
+                msg = msg[:-len(self.substring)]
+                return msg
+    
+    async def compress_convo(self, msg):
+        msg = json.loads(msg)
+        _question_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+        Chat History:
+        {}
+        Follow Up Question:
+        {}
+        Standalone Question:"""
+
+        chat_history = "\n".join(["{}: {}".format(m["role"], m["content"]) for m in msg[:-1]])
+        follow_up_question = "{}: {}".format(msg[-1]["role"], msg[-1]["content"])
+
+        response = await self.async_client.generate(model="llama3-temp0-Q6", prompt=_question_template.format(chat_history, follow_up_question), stream=False)
+        # print("Question template: ", _question_template.format(chat_history, follow_up_question), "\n")
+        return response['response']
+
+    async def requires_rag(self, msg):
         prompt_index = msg.rfind('"content":')
         # shift past content:
         prompt_index += 10
         # remove the special characters from behind
-        response = await async_client.generate(model="llama3-temp0-Q6", prompt=msg[prompt_index:len(msg)-2], system="Provide ONLY a single word response of Yes or No, Yes if you are able to provide an accurate and full answer or No if you are unable to provide an answer", stream=False)
+        response = await self.async_client.generate(model="llama3-temp0-Q6", prompt=msg[prompt_index:len(msg)-2], system="Provide ONLY a single word response of Yes or No, Yes if you are able to provide an accurate and full answer or No if you are unable to provide an answer", stream=False)
 
-        if response['response'] != "Yes":
-            msg = msg[1:]
-            question_embedding = model.encode([msg[prompt_index:len(msg)-2]])
-            best_matches = KnnSearch(question_embedding, all_embeddings, k=5) # Play with this value, higher seems to result in inaccurate results
-
-            #construct the embedding text
-            sourcetext = ""
-            for i, (index, source_text) in enumerate(best_matches, start=1):
-                sourcetext += f"{i}. Index: {index}, Source Text: {source_text}"
-            
-            # add on the system prompt to the front of the msg
-            sys_prompt_str = '[{"role": "system", "content": "Try your best to answer the questions with your own knowledge and the context of the conversation, only if that is not possible then use the following information:' + sourcetext + '"},'
-            msg = sys_prompt_str + msg
-
-        data = json.loads(msg)
-
-        # Disabling Nagle's algorithm for the socket
-        writer.get_extra_info('socket').setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-
-        # tinyllama:1.1b-chat-v0.6-q6_K
-        # llama3:8b-instruct-q6_K
-        async for part in await async_client.chat(model='llama3.1:8b-instruct-q8_0', messages=data, stream=True, keep_alive="15m"): # llama3:8b-instruct-q6_K
-            temp = part['message']['content']
-            writer.write(temp.encode('utf-8'))
-            print(temp)
-            await writer.drain()
-        
+        return False if response['response'] == "Yes" else True
+    
+    def display_ollama_performance(self, part):
         token_count = part['eval_count']
         time_ns = part['eval_duration']
         print("Tokens per second: ", int(token_count) / (float(time_ns) / (1 * 10**9)))
         print("Token Count: ", int(token_count))
         print("Time (s): ", int(time_ns) / (1 * 10**9))
 
+    async def handle_client(self, reader, writer):
+        print("msg received")
+        
+        # Wait for user input from frontend
+        msg = await self.await_user_prompt(reader)
+
+        # Compress the entire conversation + latest user prompt into a single prompt
+        compressed_prompt = await self.compress_convo(msg)
+
+        print("Compressed prompt: ", compressed_prompt, "\n")
+
+        # Check if RAG is required based on the latest compressed prompt
+        if await self.requires_rag(compressed_prompt):
+            print("User's prompt requires RAG")
+
+            # Remove starting brace "{" and encode user prompt for knn
+            msg = msg[1:]
+            prompt_index = msg.rfind('"content":')
+            question_embedding = model.encode([msg[prompt_index:len(msg)-2]])
+            best_matches = KnnSearch(question_embedding, all_embeddings, k=5) # Play with this value, higher seems to result in inaccurate results
+
+            # Construct the embedding text
+            sourcetext = ""
+            for i, (index, source_text) in enumerate(best_matches, start=1):
+                sourcetext += f"{i}. Index: {index}, Source Text: {source_text}"
+            
+            # Add on the system prompt to the front of the msg
+            _system_prompt = """Try your best to answer the question with your own knowledge and the context of the conversation before referencing the System Prompt section.
+            Follow the rules listed below when providing a response.
+
+            Rules:
+            1) If the question includes a request for code, provide a code block directly from the System Prompt section.
+            2) If you do not have the information to a question, just say this - Hmm, I'm not too sure. Do not make up an answer.
+            3) If the question includes a request for text information, attempt to answer the question with the System Prompt.
+            4) If you have information on where the code example or text information was obtained from, provide it at the end of the response.
+
+            System Prompt: """
+            sys_prompt_str = '[{"role": "system", "content": "Try your best to answer the questions with your own knowledge and the context of the conversation, only if that is not possible then use the following information:' + sourcetext + '"},'
+            #sys_prompt_str = '[{"role": "system", "content": "' + repr(_system_prompt)[1:-1] + sourcetext + '"},'
+            #print("Updated system prompt message: ", sys_prompt_str)
+            msg = sys_prompt_str + msg
+            print("Concatenated message before json:\n", msg)
+
+        data = json.loads(msg)
+
+        # Disabling Nagle's algorithm for the socket
+        writer.get_extra_info('socket').setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
+        # Stream the LLM response
+        async for part in await self.async_client.chat(model='mistral-nemo:12b-instruct-2407-q6_K', messages=data, stream=True, keep_alive="15m"): # llama3:8b-instruct-q6_K    |   mistral-nemo:12b-instruct-2407-q6_K    |    llama3.1:8b-instruct-q8_0
+            temp = part['message']['content']
+            writer.write(temp.encode('utf-8'))
+            await writer.drain()
+
+        # Display Token Speed
+        self.display_ollama_performance(part)
+        
         writer.close()
         await writer.wait_closed()
 
